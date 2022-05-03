e8d6b1b657f48002984c98870e4d382c6bf2aa54
Artemiy Sadriev <mosin1898@mail.ru>
2022-05-03 14:32:11 +0300

    initial commit

---
+++ DiffBetweenWritelnAndWriteConsoleApp/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DiffBetweenWritelnAndWriteConsoleApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("With Console.Write");
+            Console.Write("Hello ");
+            Console.Write(" World ");
+            Console.Write(" !!!");
+            Console.WriteLine("");
+            Console.WriteLine("-----------------------------------------------------------");
+            Console.WriteLine("With Console.WriteLine");
+            Console.WriteLine("Hello");
+            Console.WriteLine("World");
+            Console.WriteLine("!!!");
+            Console.ReadLine();
+        }
+    }
+}
